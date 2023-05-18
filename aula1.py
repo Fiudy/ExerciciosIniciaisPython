9a44d6eb6cc516419294891acd00b51ee07dcc70
Guilherme Carvalho <89918329+Fiudy@users.noreply.github.com>
2023-05-18 18:31:53 -0300

    Calculadora.py

--- aula1.py
+++ aula1.py
@@ -1,33 +1,31 @@
-# #Criei uma variavel do tipo string
-# #variaveis do tipo str são caracteres
-# nome = "Guilherme"
-# print(nome)
 
 
-# n1 = int(input("Informe o primero número"))
-# print(n1)
-# n2 = int(input("Informe o segundo número"))
-# print(n2)
-# operacao = input("Escolha uma operação: +, -, /")
+
+n1 = int(input("Informe o primero número"))
+print(n1)
+n2 = int(input("Informe o segundo número"))
+print(n2)
+operacao = input("Escolha uma operação: +, -, /")
 
 
-# if operacao == '+':
-#     print('{} + {}'.format(n1, n2))
-#     print(n1+n2)
+if operacao == '+':
+    print('{} + {}'.format(n1, n2))
+    print(n1+n2)
 
 
-# elif operacao == '-':
-#      print('{} - {}'.format(n1, n2))
-#      print(n1-n2)
+elif operacao == '-':
+     print('{} - {}'.format(n1, n2))
+     print(n1-n2)
 
-# elif operacao == '/':
-#     print('{} / {}'.format(n1, n2))
-#     print(n1/n2)
+elif operacao == '/':
+    print('{} / {}'.format(n1, n2))
+    print(n1/n2)
 
-# else:
-#     print('Você está usando uma operação inválida')
+else:
+    print('Você está usando uma operação inválida')
 
 
-primeiro = "Eu sou"
-segundo = "lindo"
-print(f"{primeiro} {segundo}")
+
+
+
+
