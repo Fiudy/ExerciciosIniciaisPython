f8debcf655018578b7bb51026e68253700e63f8d
Guilherme Carvalho <89918329+Fiudy@users.noreply.github.com>
2023-05-18 18:28:54 -0300

    Add files via upload

---
+++ pratica1.py
@@ -0,0 +1,8 @@
+#1 - Faça um proĀrama que converta metros para centímetros.
+
+m = int(input("Informe o valor em metros para ser convertido"))
+print(m)
+
+c = (m*100)
+
+print(f"O valor convertido é: {c}cm")
