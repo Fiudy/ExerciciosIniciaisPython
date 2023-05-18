2bb72cfc5bdd9b13c7b9b48e7322369041285c1f
Guilherme Carvalho <89918329+Fiudy@users.noreply.github.com>
2023-05-18 18:23:22 -0300

    pratica2.py

--- pratica2.py
+++ pratica2.py
@@ -1,3 +1,6 @@
+# 2 - Faça um proĀrama que receba um valor em Āraus Celsius e
+# converta para Fahrenheit, mas calma! use essa ÿórmula para
+# concluir o proĀrama. F = (C*9/5) + 3
 C = int(input('Digite o valor em graus cujo será convertido para Fahrenheit: '))
 print(C)
 F = (C*9/5) + 32
