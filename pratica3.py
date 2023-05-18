b13a7ec615379c4465257957868b8f72edd01258
Guilherme Carvalho <89918329+Fiudy@users.noreply.github.com>
2023-05-18 18:24:03 -0300

    Add files via upload

---
+++ pratica3.py
@@ -0,0 +1,17 @@
+# #3 - Tendo como dado de entrada a altura (h) de uma pessoa,
+# construa um alĀoritmo que calcule seu peso ideal, utilizando
+# as seĀuintes ÿórmulas:
+# a - Para homens: (72.7*h) - 58
+# b -Para mulheres: (62.1*h) - 44.7
+
+h = float(input("Informe sua altura: "))
+print(h)
+
+genero = input("Você é 'homem' ou 'mulher'?")
+print(genero)
+
+if genero == 'mulher':
+    print((62.1*h) - 44.7)
+
+else:
+    print((72.7*h) - 58)
