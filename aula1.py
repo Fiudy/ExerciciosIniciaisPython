8540deec6a4fb706e07fb2416dff85440a23fc0a
Guilherme Carvalho <89918329+Fiudy@users.noreply.github.com>
2023-05-18 18:05:38 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,33 @@
+# #Criei uma variavel do tipo string
+# #variaveis do tipo str são caracteres
+# nome = "Guilherme"
+# print(nome)
+
+
+# n1 = int(input("Informe o primero número"))
+# print(n1)
+# n2 = int(input("Informe o segundo número"))
+# print(n2)
+# operacao = input("Escolha uma operação: +, -, /")
+
+
+# if operacao == '+':
+#     print('{} + {}'.format(n1, n2))
+#     print(n1+n2)
+
+
+# elif operacao == '-':
+#      print('{} - {}'.format(n1, n2))
+#      print(n1-n2)
+
+# elif operacao == '/':
+#     print('{} / {}'.format(n1, n2))
+#     print(n1/n2)
+
+# else:
+#     print('Você está usando uma operação inválida')
+
+
+primeiro = "Eu sou"
+segundo = "lindo"
+print(f"{primeiro} {segundo}")
---
+++ pratica2.py
@@ -0,0 +1,6 @@
+C = int(input('Digite o valor em graus cujo será convertido para Fahrenheit: '))
+print(C)
+F = (C*9/5) + 32
+
+
+print(f'O valor convertido para Fahrenheit é: {F}°F')
