079908772875f6a342397aa0eeae0308e20d7f81
Guilherme Carvalho <89918329+Fiudy@users.noreply.github.com>
2023-05-23 18:47:27 -0300

    Add files via upload

---
+++ dado.py
@@ -0,0 +1,19 @@
+import random
+
+def rolar_dado():
+    return random.randint(1, 6)
+
+while True:
+    input("Pressione Enter para rolar o dado...")
+    resultado = rolar_dado()
+    print("O resultado do dado é:", resultado)
+    if resultado >= 5:
+     print("Número Alto")
+     
+
+    elif resultado >=3 and resultado < 4:
+        print("Número Mediano")
+    
+    else:
+        print("Número baixo!")
+    
